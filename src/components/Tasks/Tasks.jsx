4240d2ddfefd82ea7cdb9d6cb654ef3dd61ad469
akinteva_pe <akinteva_pe@niisva.org>
2022-11-24 20:51:31 +0300

    сделала разделение по типам задач

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,15 +1,50 @@
 import s from './Tasks.module.css';
 import React from 'react';
-import TaskList from "./TaskList/TaskList";
-
-const Tasks = (props) =>{
-    return(
-
-        <TaskList tasksItem={props.tasksPage.tasksItem}/>
-
-    );
 
 
+const Tasks = (props) => {
+
+
+    return (
+
+        <div className={s.tasksWrapper}>
+
+            <div className={s.btnWrapper}>
+                <button>Создать задачу</button>
+            </div>
+
+
+
+            <div className={s.tasks}>
+
+            {props.tasksPage.map(board =>
+
+                <div className={s.board}>
+
+                    <div className={s.boardTitle}>{board.name}</div>
+
+                    {board.items.map(item =>
+                        <div className={s.item}>
+                            <div className={s.itemDesc}>{item.description}</div>
+                            <div className={s.itemBtn}>
+                                <ul></ul>
+                            </div>
+
+                        </div>
+
+                    )}
+                </div>
+            )}
+            </div>
+        </div>
+        );
 }
 
 export default Tasks;
+
+
+
+
+
+
+
--- src/state.js
+++ src/state.js
@@ -1,12 +1,10 @@
 let state = {
 
-    tasksPage: {
-        tasksItem: [
-            {id: 1, description: 'Описание задачи'},
-            {id: 2, description: 'Описание задачи'},
-            {id: 3, description: 'Описание задачи'}
-        ]
-    }
+    tasksPage: [
+        {id: 1, name: 'Открытые', items: [ {id: 1, description: 'Описание задачи'}, {id: 2, description: 'Описание задачи2'}, {id: 3, description: 'Описание задачи3'} ] },
+        {id: 2, name: 'Закрытые', items: [ {id: 1, description: 'Описание задачи'}, {id: 2, description: 'Описание задачи2'}] },
+        {id: 3, name: 'Завершенные', items: [ {id: 1, description: 'Описание задачи'}, {id: 2, description: 'Описание задачи2'}] }
+    ]
 
 }
 
