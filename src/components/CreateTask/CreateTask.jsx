b73b889c9498f996ef5261be146efb3009dc7390
PovarIsSFEDU <plukash@sfedu.ru>
2022-11-26 02:28:17 +0300

    Started task_creation integration.

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -1,48 +1,71 @@
 import s from './CreateTask.module.css'
 import React from 'react';
+import axios from "axios";
+import {API_URL} from "../../services/user.service";
+import authHeader from "../../services/auth-header";
+import AuthService from "../../services/auth.service";
 
 
 const CreateTask = () => {
-    return(
+
+    let task = {
+        name: "",
+        description: "",
+        media_contents: "",
+        assignee_ids: [],
+        points: 0,
+        est_time: "",
+        subscribed_ids: [
+            AuthService.getCurrentUser().id
+        ],
+        triggers: [
+            {
+                trigger_type: "",
+                needed_action: "",
+                parent_task_id: 0,
+                author_id: AuthService.getCurrentUser().id,
+                timer: ""
+            }
+        ]
+    };
+
+    function create_task() {
+        console.log(task)
+        // return axios.post(API_URL + 'create/task', task, {headers: authHeader()});
+    }
+
+    return (
 
         <div className={s.wrapper}>
             <div className={s.content}>
-                    <div className={s.header}>
-                        <span className={s.title}>Создать задачу</span>
-                    </div>
-                        <input type="text" placeholder='Заголовок' className={s.Input} />
-                            <select className={s.Input}>
-                                <option disabled selected>Способ оповещания:</option>
-                                <option>Почта</option>
-                                <option>Телефон</option>
-                                <option>СМС</option>
-                            </select>
+                <div className={s.header}>
+                    <span className={s.title}>Создать задачу</span>
+                </div>
+                <input type="text" placeholder='Заголовок' className={s.Input}
+                       onChange={(e) => task.name = e.target.value}/>
 
-                        <input type="number" placeholder='Очки за выполнение' className={s.Input}/>
+                <input type="number" placeholder='Очки за выполнение' className={s.Input}
+                       onChange={(e) => task.points = e.target.value}/>
 
-                        <div className={s.Data}>
-                            <div>
-                                <span>Конец:</span>
-                                <input type="date" className={s.inputModal}/>
-                            </div>
+                <input type="text" placeholder='Время выполнения' className={s.Input}
+                       onChange={(e) => task.est_time = e.target.value}/>
 
-                        </div>
-
-                        <textarea className={s.Input} placeholder='Добавьте описание задачи'/>
-                    <div className={s.addUser}>
-                        <p>Назначить исполнителя:</p>
-                        <input type='text' className={s.Input}/>
-                        <button className={s.btnStyle}>Добавить</button>
-                    </div>
+                <textarea className={s.Input} placeholder='Добавьте описание задачи'
+                          onChange={(e) => task.description = e.target.value}/>
+                <div className={s.addUser}>
+                    <p>Назначить исполнителя:</p>
+                    <input type='text' className={s.Input}/>
+                    <button className={s.btnStyle}>Добавить</button>
+                </div>
 
                 <div className={s.triggerBtn}>
                     <button className={s.btnStyle}>Открыть модалку с триггерами</button>
                 </div>
 
                 <div className={s.btnSection}>
-                    <button className={s.btnStyle}>Создать</button>
+                    <button className={s.btnStyle} onClick={create_task}>Создать</button>
                 </div>
-        </div>
+            </div>
         </div>
 
     );
